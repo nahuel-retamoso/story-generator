2e4d9a8dbc96f7709614d2a167fe30cb80480ea5
nahuel-retamoso <nahuel.retamoso@gmail.com>
2023-06-16 18:00:35 -0300

    first commit

---
+++ app/api/generate/route.js
@@ -0,0 +1,12 @@
+// import { Configuration, OpenAIApi } from "openai";
+
+// const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+// const configuration = new Configuration({
+//     apiKey: OPENAI_API_KEY,
+// })
+// const openai = new OpenAIApi(configuration);
+
+export async function POST () {
+  console.log('hola')
+}
